fa2d217ef29c152bd5f17dab668526be0c32a2d3
Jarbas Santos <jarbells@outlook.com>
2024-06-28 18:24:45 -0300

    Versão do professor com melhorias.

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,9 +4,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Product;
 
@@ -29,16 +31,20 @@ public class Program {
 					productsList.add(new Product(fields[0], Double.parseDouble(fields[1])));
 					line = br.readLine();
 				}
-				double sum = productsList.stream()
-						.mapToDouble(Product::getPrice)
-						.reduce(0.0, Double::sum);
+				
+				double average = productsList.stream()
+						.mapToDouble(Product::getPrice) // .map(p -> p.getPrice) 
+						.reduce(0.0, Double::sum) / productsList.size(); // .reduce(0.0, (x,y) -> x + y) / list.size();
+				
 				if (!productsList.isEmpty()) {
-					double average = sum / productsList.size();
+					Comparator<String> comparator = (str1, str2) -> str1.toUpperCase().compareTo(str2.toUpperCase());
+					
 					System.out.printf("Average price: %.2f%n", average);
-					List<String> prod = productsList.stream()
+					
+					List<String> productNames = productsList.stream()
 							.filter(x -> x.getPrice() <= average)
-							.map(x -> x.getName()).sorted().toList().reversed();
-					prod.forEach(System.out::println);
+							.map(x -> x.getName()).sorted(comparator.reversed()).collect(Collectors.toList());
+					productNames.forEach(System.out::println);
 				}				
 			}
 			catch (IOException e) {
